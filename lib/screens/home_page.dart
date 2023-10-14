9a09fd75e3cf278086eafc1f24fed51c33310103
adel7ussein <aadelshabib67009@gmail.com>
2023-10-15 02:06:53 +0300

    first commit

---
+++ lib/screens/home_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:store_app/models/product_model.dart';
+import 'package:store_app/services/get_all_product_service.dart';
+
+import '../widgets/custom_card.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  static String id = 'HomePage';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){}, icon: const Icon(FontAwesomeIcons.cartPlus,
+          color: Colors.black,))
+        ],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: const Text('New Trend',style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(right: 16, left: 16, top: 65),
+        child: FutureBuilder<List<ProductModel>>(future: AllProductsService().getAllProducts(),builder: (context, snapshot){
+          if (snapshot.hasData) {
+            List<ProductModel> products = snapshot.data!;
+                    return GridView.builder( itemCount: products.length,clipBehavior: Clip.none,gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,
+                        childAspectRatio: 1.6, crossAxisSpacing: 10, mainAxisSpacing: 100
+                               ), itemBuilder: (context ,index){
+                                         return  CustomCard(product: products[index],);
+                                   });
+                    }else{
+                      return const Center(child: CircularProgressIndicator());
+                    }
+        })
+      )
+      );
+  }
+}
